e35efdebd3556bb296ff2fe05c11c2e958290d1c
Devansh27112001 <dkansara2711@gmail.com>
2024-05-10 09:01:44 -0400

    static layout done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
 export default function App() {
-  return <div className="App">This is the new project</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜You have X items in your list, and you already have X (X%)</em>
+    </footer>
+  );
 }
